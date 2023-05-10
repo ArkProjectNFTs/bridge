d4fff82420fc832af9e0bbe056c7fa76e2226a48
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-05-10 18:47:16 +0200

    chore(blockchain): update hardhat configuration (#104)
    * chore(blockchain): update hardhat config
    
    * chore(blockchain): update hardhat config
    
    * chore: update github workflow
    
    * chore(blockchain): add cairo 1.0 hardhat config
    
    * feat(blockchain): add scarb config
    
    * ci: add scarb workflow

--- apps/blockchain/scripts/ethereum/deployContract.ts
+++ apps/blockchain/scripts/ethereum/deployContract.ts
@@ -34,7 +34,7 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main().catch(error => {
   console.error(error);
   process.exitCode = 1;
 });
