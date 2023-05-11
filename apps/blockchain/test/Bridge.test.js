56d185e1c6e4626d5c9f38ddeb660f6876b5346a
Rémi Roycourt <remiroycourt@gmail.com>
2023-05-11 16:45:30 +0200

    test: bridge contract

--- apps/blockchain/test/Bridge.test.js
+++ apps/blockchain/test/Bridge.test.js
@@ -4,14 +4,14 @@ const { expect } = require("chai");
 describe("Bridge", function () {
   let bridge;
   let bridgeEscrow;
-  let erc721Contract;
+  let nftCollection;
 
+  let admin;
   let owner;
-  let addr1;
   let addr2;
 
   beforeEach(async function () {
-    [owner, addr1, addr2] = await ethers.getSigners();
+    [admin, owner, addr2] = await ethers.getSigners();
 
     const starknetCoreFactory = await ethers.getContractFactory("TestStarknetCore");
     const starknetCore = await starknetCoreFactory.deploy();
@@ -26,8 +26,8 @@ describe("Bridge", function () {
     await bridgeEscrow.grantBridgeRole(bridge.address);
 
     const erc721ContractFactory = await ethers.getContractFactory("TestERC721");
-    erc721Contract = await erc721ContractFactory.deploy("Test", "Test");
-    await erc721Contract.deployed();
+    nftCollection = await erc721ContractFactory.deploy("Test", "Test");
+    await nftCollection.deployed();
   });
 
   it("should have the right access role on the escrow contract", async () => {
@@ -36,9 +36,11 @@ describe("Bridge", function () {
     expect(hasRole).to.equal(true);
   });
 
-  //   it("should deposit token from L1", async () => {
-  //     const l2OwnerAddress = "0x07a096EcAa08A3a50dc2E1283C38586C497e0e684648aB2ABe02427E2aFe1e77";
-  //     const tokenId = 0;
-  //     await bridge.depositTokenFromL1ToL2(erc721Contract.address, l2OwnerAddress, tokenId);
-  //   });
+  it("should deposit token from L1", async () => {
+    // const l2OwnerAddress = "0x07a096EcAa08A3a50dc2E1283C38586C497e0e684648aB2ABe02427E2aFe1e77";
+    const tokenId = 10;
+    await nftCollection.approve(bridge.address, tokenId);
+    await nftCollection.mint(owner.address, tokenId);
+    await bridge.depositTokenFromL1ToL2(nftCollection.address, 0x0, tokenId);
+  });
 });
