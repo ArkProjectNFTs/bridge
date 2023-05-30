70ab664fcd4f215d451953957a41fc7b423bb30c
Yohan Tancrez <yohantancrez@gmail.com>
2023-05-30 17:45:25 +0200

    feat: select nft + drawer

--- apps/web/app/components/ConnectStarkNetModal.tsx
+++ apps/web/app/components/ConnectStarkNetModal.tsx
@@ -39,7 +39,11 @@ export default function ConnectStarkNetModal({
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog
+          as="div"
+          className="relative z-10 text-black"
+          onClose={closeModal}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
