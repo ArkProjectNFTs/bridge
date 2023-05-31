afbf6f9a478bb267f79307179c42453def6cc0f0
TANCREZ Yohan <yohantancrez@gmail.com>
2023-05-31 11:20:20 +0200

    chore(web): used `create-t3-app` to bootstrap the project and adapted previous components (#113)
    * used create-t3-app to bootstrap the project and adapted previous components
    
    * chore: some things to make review easier
    
    * chore: some things to make review easier
    
    * chore: added back MIT license
    
    * removed DATABASE_URL requirement in `env.mjs`

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,9 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
--- apps/web/ui/DefaultTags.tsx
+++
@@ -1,9 +0,0 @@
-// Default <head> tags we want shared across the app
-export function DefaultTags() {
-  return (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-    </>
-  );
-}
