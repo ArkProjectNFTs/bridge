293e11af63bada28eda1a9d90c60c88bac363839
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-06-08 08:55:07 +0200

    feat(ethereum-contracts): bridge contract (#106)
    * feat(ethereum-contracts): create bridge escrow contract
    
    * feat(contracts): bridge
    
    * test: bridge contract
    
    * test: bridge contract
    
    * test: bridge contract
    
    * feat(contracts): bridge escrow
    
    * fix(contracts): remove force withdraw
    
    * feat(contracts): add deposit event
    
    * feat(solidity-contracts): check l2 address
    
    * feat(solidity-contracts): add is open check
    
    * fix(cairo-contracts): deposit test

--- apps/blockchain/contracts/ethereum/TestBridge.sol
+++ apps/blockchain/contracts/ethereum/TestBridge.sol
@@ -7,25 +7,21 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "./interfaces/IBridgeEscrow.sol";
 
 contract TestBridge is Ownable {
-    IBridgeEscrow escrowContract;
+  IBridgeEscrow escrowContract;
 
-    constructor(address ecrowContractAddress) {
-        escrowContract = IBridgeEscrow(ecrowContractAddress);
-    }
+  constructor(address ecrowContractAddress) {
+    escrowContract = IBridgeEscrow(ecrowContractAddress);
+  }
 
-    function deposit(
-        address tokenAddress,
-        uint tokenId,
-        address depositorAddress
-    ) public {
-        escrowContract.depositNFT(tokenAddress, tokenId, depositorAddress);
-    }
+  function deposit(address tokenAddress, uint tokenId, address depositorAddress) public {
+    escrowContract.depositNFT(tokenAddress, tokenId, depositorAddress);
+  }
 
-    function withdraw(address tokenAddress, uint tokenId) public {
-        escrowContract.withdrawDeposit(tokenAddress, tokenId);
-    }
+  function withdraw(address tokenAddress, uint tokenId) public {
+    escrowContract.withdrawDeposit(tokenAddress, tokenId);
+  }
 
-    function cancel(address tokenAddress, uint tokenId) public {
-        escrowContract.cancelDeposit(tokenAddress, tokenId);
-    }
+  function cancel(address tokenAddress, uint tokenId) public {
+    escrowContract.cancelDeposit(tokenAddress, tokenId);
+  }
 }
