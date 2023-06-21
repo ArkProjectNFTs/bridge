697d7b64e839c95cac043e35ab4d457eee0d218b
Yohan Tancrez <yohantancrez@gmail.com>
2023-06-21 02:32:39 +0200

    removed unnecessary comment in tailwind config

--- packages/design-system/tailwind.config.ts
+++ packages/design-system/tailwind.config.ts
@@ -3,6 +3,5 @@ import sharedConfig from "tailwind-config/tailwind.config";
 
 export default {
   content: sharedConfig.content,
-  // prefix: "ds-",
   presets: [sharedConfig],
 } satisfies Config;
