d7d9567b440ebf54ffb2d6a1a2fcac602dda8e88
glihm <dev@glihm.net>
2023-07-02 13:47:02 -0600

    add pre-defined katana account for starkli

--- apps/blockchain/contracts/starknet/deploy.sh
+++ apps/blockchain/contracts/starknet/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# export STARKNET_ACCOUNT=~/.accounts/katana_200rc6_1
-# export STARKNET_RPC=http://0.0.0.0:5050
-# export STARKNET_KEYSTORE=~/.keystore/katana_200rc6_1
+STARKLI=~/starknet/starkli/target/release/starkli
+
+export STARKNET_ACCOUNT=./katana_account.json
+export STARKNET_RPC=http://0.0.0.0:5050
+export STARKNET_KEYSTORE=./katana_key.json
 
 ADMIN_ACCOUNT_ADDR=0x03ee9e18edc71a6df30ac3aca2e0b02a198fbce19b7480a63a0d71cbd76652e0
 
@@ -13,12 +15,12 @@ ERC721_CLASS_HASH=0x021ac711d5795b5b86cae6fb1d045f6ebc0c327ba21a3581cb25ebf3e5a6
 # TODO: when startkli address book is out, don't need this anymore.
 BRIDGE_ADDR=0x026fb840546e842f4a858a5b5ea2bfcfeabfdefa2705f56c13d16d966d0912c6
 
-starkli declare --compiler-version 2.0.0-rc6 ./target/dev/starklane_bridge.sierra.json --keystore-password 1234
-starkli declare --compiler-version 2.0.0-rc6 ./target/dev/starklane_erc721_bridgeable.sierra.json --keystore-password 1234
+$STARKLI declare --compiler-version 2.0.0-rc6 ./target/dev/starklane_bridge.sierra.json --keystore-password 1234
+$STARKLI declare --compiler-version 2.0.0-rc6 ./target/dev/starklane_erc721_bridgeable.sierra.json --keystore-password 1234
 
 # Deploy bridge, with admin addr.
 echo "*** DEPLOYING Bridge"
-starkli deploy "${BRIDGE_CLASS_HASH}" "${ADMIN_ACCOUNT_ADDR}" --keystore-password 1234
+$STARKLI deploy "${BRIDGE_CLASS_HASH}" "${ADMIN_ACCOUNT_ADDR}" --keystore-password 1234
 
 # Deploy ERC721, with bridge addr and admin and admin account as collection owner.
 # everai
@@ -27,7 +29,7 @@ TOKEN_NAME=0x657665726169
 TOKEN_SYMBOL=0x44554f
 
 echo "*** DEPLOYING ERC721Bridgeable"
-starkli deploy --keystore-password 1234 "${ERC721_CLASS_HASH}" \
+$STARKLI deploy --keystore-password 1234 "${ERC721_CLASS_HASH}" \
         1 "${TOKEN_NAME}" \
         1 "${TOKEN_SYMBOL}" \
         "${BRIDGE_ADDR}" \
