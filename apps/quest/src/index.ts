cbd33ee8aa63cada1810421e18e00181d50eb062
Rémi Roycourt <remiroycourt@gmail.com>
2024-03-13 11:18:14 +0100

    feat(quest): update cron

--- apps/quest/src/index.ts
+++ apps/quest/src/index.ts
@@ -1,9 +1,12 @@
+import { config } from "dotenv";
 import cron from "node-cron";
 import { Pool } from "pg";
 import { MongoClient } from "mongodb";
 
 const EVERAI_L1_CONTRACT_ADDRESS = "0x246d8e00a48576f24a274fb7d35a7d1c7ef950a7";
 const QUEST_TYPE = "BRIDGE_EVERAI";
+
+config();
 
 interface BridgeRequest {
   _id: string;
@@ -13,16 +16,10 @@ interface BridgeRequest {
   collection_src: string;
 }
 
-interface User {
-  id: number;
-  starknetAccountAddress: string;
-  starknetAccountAddressCreatedAt: Date;
-}
-
-interface Quest {
+interface BridgeQuest {
   id: number;
   questType: string;
-  userId: number;
+  starknetAccountAddress: string;
   createdAt: Date;
   updatedAt: Date;
   count: number;
@@ -37,80 +34,81 @@ async function handleBridgeRequest(doc: BridgeRequest) {
   try {
     const client = await pool.connect();
 
-    // Find or create user (combined logic)
-    const userQuery = `
-      INSERT INTO User (starknetAccountAddress, starknetAccountAddressCreatedAt)
-      VALUES ($1, now())
-      ON CONFLICT (starknetAccountAddress) DO UPDATE SET
-        starknetAccountAddressCreatedAt = EXCLUDED.starknetAccountAddressCreatedAt
-      RETURNING id;
-    `;
-
-    const userValues: [string] = [doc.to];
-    const userResult = await client.query<User>(userQuery, userValues);
-
-    const userId = userResult.rows[0].id;
-
-    // Check and update quest (combined logic)
     const questQuery = `
-      SELECT * FROM Quest
-      WHERE userId = $1 AND questType = $2 FOR UPDATE SKIP LOCKED;
+      SELECT * FROM public."BridgeQuest"
+      WHERE "starknetAccountAddress" = $1 AND "questType" = $2 FOR UPDATE SKIP LOCKED;
     `;
 
-    const questValues: [number, string] = [userId, QUEST_TYPE];
-    const questResult = await client.query<Quest>(questQuery, questValues);
+    const questValues = [doc.to, QUEST_TYPE];
+    const questResult = await client.query<BridgeQuest>(
+      questQuery,
+      questValues,
+    );
 
     if (questResult.rowCount === 0) {
-      // Insert new quest
       const insertQuery = `
-        INSERT INTO Quest (questType, userId, createdAt, updatedAt, count, questData)
+        INSERT INTO public."BridgeQuest" ("questType", "starknetAccountAddress", "createdAt", "updatedAt", "count", "questData")
         VALUES ($1, $2, now(), now(), 1, $3)
         RETURNING *;
       `;
 
-      const insertValues: [string, number, any] = [QUEST_TYPE, userId, {}];
-      const insertResult = await client.query<Quest>(insertQuery, insertValues);
+      const insertValues: [string, string, any] = [
+        QUEST_TYPE,
+        doc.to,
+        { tokenIds: doc.token_ids },
+      ];
+      const insertResult = await client.query<BridgeQuest>(
+        insertQuery,
+        insertValues,
+      );
 
       console.log("Quest inserted successfully:", insertResult.rows[0]);
     } else {
-      // Update existing quest
       const updateQuery = `
-        UPDATE Quest
-        SET count = count + 1, updatedAt = now()
-        WHERE id = $1;
+        UPDATE public."BridgeQuest"
+        SET count = count + 1, "updatedAt" = now(), "questData" = $3
+        WHERE "questType" = $1 AND "starknetAccountAddress" = $2;
       `;
 
-      const updateValues: [number] = [questResult.rows[0].id];
-      await client.query(updateQuery, updateValues);
+      const data = questResult.rows[0];
 
-      console.log("Quest updated successfully:", questResult.rows[0]);
+      await client.query(updateQuery, [
+        data.questType,
+        data.starknetAccountAddress,
+        {
+          ...data.questData,
+          tokenIds: [
+            ...(data.questData.tokenIds || []),
+            ...(doc.token_ids || []),
+          ],
+        },
+      ]);
+
+      console.log("Quest updated successfully:", data);
     }
 
     await client.release();
+
+    const mongoClient = new MongoClient("mongodb://172.19.0.2:27017");
+    await mongoClient.connect();
+    const db = mongoClient.db("starklane");
+    const requests = db.collection<BridgeRequest>("starknet_bridge_requests");
+    await requests.updateOne({ _id: doc._id }, { $set: { is_checked: true } });
+    await mongoClient.close();
   } catch (error) {
     console.error("Error handling bridge request:", error);
   } finally {
     pool.end();
   }
-
-  const mongoClient = new MongoClient("mongodb://172.19.0.2:27017");
-  await mongoClient.connect();
-  const db = mongoClient.db("starklane");
-  const requests = db.collection<BridgeRequest>("starknet_bridge_requests");
-  await requests.updateOne({ _id: doc._id }, { $set: { is_checked: true } });
-  await mongoClient.close();
 }
 
 async function tick() {
   const uri = "mongodb://172.19.0.2:27017";
   const client = new MongoClient(uri);
 
-  console.log("Connecting to mongodb...");
-
   try {
+    console.log("Checking bridge requests...");
     await client.connect();
-    console.log("Connecté à MongoDB");
-
     const db = client.db("starklane");
 
     const requests = db.collection<BridgeRequest>("starknet_bridge_requests");
@@ -130,6 +128,5 @@ async function tick() {
 }
 
 cron.schedule("*/10 * * * * *", () => {
-  console.log("Checking bridge requests...");
   tick();
 });
