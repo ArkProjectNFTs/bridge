ec15a4feebac1afe7790d2b26ff089344ab7622a
Yohan Tancrez <yohantancrez@gmail.com>
2024-03-13 02:53:11 +0100

    fixed modal bug

--- apps/web/src/app/_components/ConnectWalletsButton.tsx
+++ apps/web/src/app/_components/ConnectWalletsButton.tsx
@@ -1,17 +1,41 @@
 "use client";
-
+import { useAccount as useStarknetAccount } from "@starknet-react/core";
 import { Typography } from "design-system";
+import { useAccount as useEthereumAccount } from "wagmi";
 
 import { useConnectModals } from "./WalletModals/WalletModalsContext";
 
 export default function ConnectWalletsButton() {
-  const { toggleConnectWalletsModal } = useConnectModals();
+  const {
+    toggleConnectEthereumWalletModal,
+    toggleConnectStarknetWalletModal,
+    toggleConnectWalletsModal,
+  } = useConnectModals();
+  const { address: ethereumAddress } = useEthereumAccount();
+
+  const { address: starknetAddress } = useStarknetAccount();
+
+  function toggleConnectModal() {
+    if (starknetAddress === undefined && ethereumAddress === undefined) {
+      toggleConnectWalletsModal();
+      return;
+    }
+    if (starknetAddress === undefined) {
+      toggleConnectStarknetWalletModal();
+      return;
+    }
+    if (ethereumAddress === undefined) {
+      toggleConnectEthereumWalletModal();
+      return;
+    }
+    toggleConnectWalletsModal;
+  }
 
   return (
     <>
       <button
         className="mx-auto flex h-12 w-full items-center justify-center gap-2.5 rounded-full bg-space-blue-source px-9 dark:bg-space-blue-source sm:w-auto"
-        onClick={toggleConnectWalletsModal}
+        onClick={toggleConnectModal}
       >
         <Typography
           className="text-white dark:text-black"
