3aec977205e4deb898b2ad38021c67485dfa5014
Patrice Tisserand <p.tisserand@gmail.com>
2024-03-14 10:04:05 +0100

    indexer: fix address padding issue.

--- apps/indexer/src/handlers/requests.rs
+++ apps/indexer/src/handlers/requests.rs
@@ -4,17 +4,41 @@ use axum::{
     Json,
 };
 use serde::{Deserialize, Serialize};
+use serde::ser::SerializeStruct;
 
 use super::AppState;
-use crate::storage::{
+use crate::{storage::{
     protocol::ProtocolParser,
     store::{EventStore, RequestStore},
     Event, Request,
-};
+}, utils::{denormalize_hex, normalize_hex}};
+
+
+#[derive(Debug, Deserialize)]
+pub struct RequestWrapper(pub Request);
+
+impl Serialize for RequestWrapper {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer {
+        let RequestWrapper(ref inner) = *self;
+        let mut state = serializer.serialize_struct("Request", 7)?;
+        state.serialize_field("hash", &inner.hash)?;
+        state.serialize_field("chain_src", &inner.chain_src)?;
+        state.serialize_field("from", &denormalize_hex(&inner.from).expect("Failed to denormalize 'from'"))?;
+        state.serialize_field("to", &denormalize_hex(&inner.to).expect("Failed to denormalize 'to'"))?;
+        state.serialize_field("collection_src", &denormalize_hex(&inner.collection_src).expect("Failed to denormalize 'collection_src'"))?;
+        state.serialize_field("collection_dst", &denormalize_hex(&inner.collection_dst).expect("Failed to denormalize 'collection_dst'"))?;
+        state.serialize_field("content", &inner.content)?;
+        state.end()
+    }
+}
+
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RequestInfo {
-    req: Request,
+    req: RequestWrapper,
     events: Vec<Event>,
     token_ids: Vec<String>,
 }
@@ -24,16 +48,18 @@ pub async fn reqs_info_from_wallet(
     Path(wallet): Path<String>,
     state: State<AppState>,
 ) -> Result<Json<Vec<RequestInfo>>, (StatusCode, String)> {
+    let wallet = normalize_hex(&wallet).expect("Wallet address shall be an hexadecimal string");
+
     let mut dtos: Vec<RequestInfo> = vec![];
 
-    if let Ok(reqs) = state.store.reqs_by_wallet(&wallet.to_lowercase()).await {
+    if let Ok(reqs) = state.store.reqs_by_wallet(&wallet).await {
         for req in reqs {
             if let Ok(mut events) = state.store.events_by_request(&req.hash).await {
                 // Sort enum to ensure the latest is the current status.
                 events.sort_by_key(|e| e.label);
 
                 dtos.push(RequestInfo {
-                    req: req.clone(),
+                    req: RequestWrapper(req.clone()),
                     events,
                     token_ids: req.get_token_ids(),
                 });
@@ -56,7 +82,8 @@ pub async fn reqs_info_from_wallet(
 }
 
 pub async fn transaction(Path(txhash): Path<String>, state: State<AppState>) -> StatusCode {
-    if let Ok(event) = state.store.event_by_tx(&txhash.to_lowercase()).await {
+    let txhash = normalize_hex(&txhash).expect("Transaction hash shall be an hexadecimal string");
+    if let Ok(event) = state.store.event_by_tx(&txhash).await {
         if event.is_some() {
             return StatusCode::OK;
         }
