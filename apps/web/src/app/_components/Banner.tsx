3f7841581c1e22e4bc78d9413247d42bc17089d4
Yohan @ ScreenshotLabs <yohantancrez@gmail.com>
2024-07-26 15:59:41 +0200

    feat: added bridge upcoming audit banner (#223)
    * feat: added bridge pause banner
    
    * fix: removed unnecessary banner
    
    * fix: lint
    
    * proper date

---
+++ apps/web/src/app/_components/Banner.tsx
@@ -0,0 +1,12 @@
+export default function Banner() {
+  return (
+    <div className="bg-space-blue-400 px-4 py-2 font-semibold">
+      <p className="text-center">
+        Important Notice: The Bridge will be temporarily unavailable from
+        07/30/2024 for at least 3 weeks due to a comprehensive audit. We
+        apologize for any inconvenience this may cause and appreciate your
+        understanding.
+      </p>
+    </div>
+  );
+}
