42c8710c8b2ff4f019153bd074a00991e2b253ca
Deon <110722148+DanielEmmanuel1@users.noreply.github.com>
2024-10-01 00:30:34 +0100

    Update IStarklaneEvent.sol

--- apps/blockchain/ethereum/src/IStarklaneEvent.sol
+++ apps/blockchain/ethereum/src/IStarklaneEvent.sol
@@ -3,43 +3,44 @@
 pragma solidity ^0.8.0;
 
 interface IStarklaneEvent {
+
     /**
-     * @notice Request initiated on L1.
-     */
+       @notice Request initiated on L1.
+    */
     event DepositRequestInitiated(uint256 indexed hash, uint256 block_timestamp, uint256[] reqContent);
 
     /**
-     * @notice Request from L2 completed.
-     */
+       @notice Request from L2 completed.
+    */
     event WithdrawRequestCompleted(uint256 indexed hash, uint256 block_timestamp, uint256[] reqContent);
 
     /**
-     * @notice A request cancellation is started
-     */
+       @notice A request cancellation is started
+    */
     event CancelRequestStarted(uint256 indexed hash, uint256 block_timestamp);
 
     /**
-     * @notice A request cancellation is completed
-     */
+       @notice A request cancellation is completed
+    */
     event CancelRequestCompleted(uint256 indexed hash, uint256 block_timestamp);
 
     /**
-     * @notice White list for deposit.
-     */
+       @notice White list for deposit.
+    */
     event WhiteListUpdated(bool enable);
 
     /**
-     * @notice White list status updated for collection.
-     */
+       @notice White list status updated for collection.
+    */
     event CollectionWhiteListUpdated(address indexed collection, bool enable);
 
     /**
-     * @notice L1 L2 collection mapping updated
-     */
+       @notice L1 L2 collection mapping updated
+    */
     event L1L2CollectionMappingUpdated(address indexed colllectionL1, uint256 indexed collectionL2);
 
     /**
-     * @notice Minimum gas fee updated
-     */
+       @notice Minimum gas fee updated
+    */
     event MinimumGasFeeUpdated(uint256 indexed gasFee);
 }
