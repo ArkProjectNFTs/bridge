5a1bd3809942eacc9efb6c20e2d54b2466890964
Deon <110722148+DanielEmmanuel1@users.noreply.github.com>
2024-10-01 10:39:44 +0100

    Update Bridge.t.sol

--- apps/blockchain/ethereum/test/Bridge.t.sol
+++ apps/blockchain/ethereum/test/Bridge.t.sol
@@ -321,60 +321,7 @@ contract BridgeTest is Test, IStarklaneEvent {
         IStarklane(bridge).withdrawTokens(reqSerialized);
     }
 
-    // Audit:
-    // [C-02] Impossible to withdraw L1 native tokens back on L1 after bridging to L2. Also, a different collection address will be generated for every native l2 token bridged to l1
-    function test_withdrawTokensERC721StarknetWithdrawDeploySameCollection() public {
-        // Build the request and compute it's "would be" message hash.
-        felt252 header = Protocol.requestHeaderV1(CollectionType.ERC721, false, false);
-        Request memory req = buildRequestDeploy(header, 888, bob);
-        uint256[] memory reqSerialized = Protocol.requestSerialize(req);
-        bytes32 msgHash = computeMessageHashFromL2(reqSerialized);
-
-        // The message must be simulated to come from starknet verifier contract
-        // on L1 and pushed to starknet core.
-        uint256[] memory msgHashes = new uint256[](1);
-        msgHashes[0] = uint256(msgHash);
-        IStarknetMessagingLocal(snCore).addMessageHashesFromL2(msgHashes);
-        address collection1 = IStarklane(bridge).withdrawTokens(reqSerialized);
-
-        assertEq(IERC721(collection1).ownerOf(888), bob);
-        // Error message from Starknet Core expected.
-        vm.expectRevert(bytes("INVALID_MESSAGE_TO_CONSUME"));
-        IStarklane(bridge).withdrawTokens(reqSerialized);
-
-        req.tokenIds[0] = 777;
-        reqSerialized = Protocol.requestSerialize(req);
-        msgHash = computeMessageHashFromL2(reqSerialized);
-
-        msgHashes[0] = uint256(msgHash);
-        IStarknetMessagingLocal(snCore).addMessageHashesFromL2(msgHashes);
-        address collection2 = IStarklane(bridge).withdrawTokens(reqSerialized);
-
-        assertEq(IERC721(collection2).ownerOf(777), bob);
-        assertEq(collection1, collection2);
-    }
-
-    function test_withdrawTokensERC721StarknetWithdrawDeployDifferentCollection() public {
-        felt252 header = Protocol.requestHeaderV1(CollectionType.ERC721, false, false);
-        Request memory req = buildRequestDeploy(header, 888, bob);
-        uint256[] memory reqSerialized = Protocol.requestSerialize(req);
-        bytes32 msgHash = computeMessageHashFromL2(reqSerialized);
-
-        // The message must be simulated to come from starknet verifier contract
-        // on L1 and pushed to starknet core.
-        uint256[] memory msgHashes = new uint256[](1);
-        msgHashes[0] = uint256(msgHash);
-        IStarknetMessagingLocal(snCore).addMessageHashesFromL2(hashes);
-        address collection = IStarklane(bridge).withdrawTokens(reqSerialized);
-
-       // TODO: add verification of event emission.
-    }
-
-assertEq(IERC721(collection).ownerOf(888), bob);
-
-// Error message from Starknet Core expected.
-vm.expectRevert(bytes("INVALID_MESSAGE_TO_CONSUME"));
-IStarklane(bridge).withdrawTokens(reqSerialized);
+ 
 
 // Audit:
 // [C-02] Impossible to withdraw L1 native tokens back on L1 after bridging to L2. Also, a different collection address will be generated for every native l2 token bridged to l1
@@ -507,7 +454,6 @@ function test_cancelRequest() public {
 
     Request memory req = Protocol.requestDeserialize(payload, 0);
 
-    vm.startPrank(alice);
     vm.expectEmit(true, false, false, false, bridge);
     emit CancelRequestStarted(req.hash, 42);
     IStarklane(bridge).startRequestCancellation(payload, nonce);
@@ -515,8 +461,6 @@ function test_cancelRequest() public {
     vm.expectEmit(true, false, false, false, bridge);
     emit CancelRequestCompleted(req.hash, 42);
     IStarklane(bridge).cancelRequest(payload, nonce);
-
-    vm.stopPrank();
 
     assert(IERC721(erc721C1).ownerOf(ids[0]) == alice);
     assert(IERC721(erc721C1).ownerOf(ids[1]) == alice);
@@ -559,8 +503,6 @@ function test_cancelRequest_withDelay() public {
 
     Request memory req = Protocol.requestDeserialize(payload, 0);
 
-    vm.startPrank(alice);
-
     vm.expectEmit(true, false, false, false, bridge);
     emit CancelRequestStarted(req.hash, 42);
     IStarklane(bridge).startRequestCancellation(payload, nonce);
@@ -570,8 +512,6 @@ function test_cancelRequest_withDelay() public {
 
     skip(delay * 1 seconds);
     IStarklane(bridge).cancelRequest(payload, nonce);
-
-    vm.stopPrank();
 
     assert(IERC721(erc721C1).ownerOf(ids[0]) == alice);
     assert(IERC721(erc721C1).ownerOf(ids[1]) == alice);
