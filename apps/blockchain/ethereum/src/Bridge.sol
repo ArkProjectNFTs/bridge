b3d1193275e9e047bd6716d4eef0568a3c192b94
Deon <110722148+DanielEmmanuel1@users.noreply.github.com>
2024-10-03 13:59:12 +0100

    Update Bridge.sol

--- apps/blockchain/ethereum/src/Bridge.sol
+++ apps/blockchain/ethereum/src/Bridge.sol
@@ -43,6 +43,8 @@ contract Starklane is
     address[] _collections;
     bool _enabled;
     bool _whiteListEnabled;
+// updatable the minimum gas fee
+    uint256 _minimumGasFee = 5e13; // 0.00005 ETH
 
     /**
      * @notice Initializes the implementation, only callable once.
